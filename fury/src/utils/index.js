84ac004dac732eafbd60f23786c29f3473352e18
nyel-101 <dandevjr@gmail.com>
2025-02-14 18:20:26 +0000

    chore(utils): optimize auth tokens

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fce7e6b54ad47e6868b6a51e83c9910 = () => {
-  return '2026-06-23T11:47:58Z';
+const node_8e9bf7f6bff24ef889fc125dfc6e909e = () => {
+  return '2025-02-14T18:20:26Z';
 };
