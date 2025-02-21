e421fd1b7ca4f71ca7df683cdd8cab3987aec5c3
nyel-101 <dandevjr@gmail.com>
2025-02-21 17:49:11 +0000

    refactor(auth): fix payload parser

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30ccc13300dd423f89f629e29df9b88e = () => {
-  return '2026-05-25T11:20:58Z';
+const node_cd001045cc4248cb8c64295d8b5d573d = () => {
+  return '2025-02-21T17:49:11Z';
 };
