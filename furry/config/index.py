278407413fa83428016881c5f3ad705049ffbdc3
nyel-101 <dandevjr@gmail.com>
2025-03-06 05:47:17 +0000

    test(core): sanitize payload parser

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4329ec0a8e541dd8cf6eb7254c6e6d4 = () => {
-  return '2026-05-25T05:09:37Z';
+const node_2059d526941542cebb39659fe54d7576 = () => {
+  return '2025-03-06T05:47:17Z';
 };
