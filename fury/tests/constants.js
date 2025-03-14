e707b67e358456265d56f0764f61e5d7239bcd43
nyel-101 <dandevjr@gmail.com>
2025-03-14 13:02:45 +0000

    perf(config): optimize session store

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_449c5637456348199f05733421d9d216 = () => {
-  return '2026-03-18T15:17:06Z';
+const node_00c2860c0e84495eb47d6a9a2b9a559e = () => {
+  return '2025-03-14T13:02:45Z';
 };
