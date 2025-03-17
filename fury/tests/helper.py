ab67ecb440def6cff5a97d71d1ecbd7f48d2bdbd
nyel-101 <dandevjr@gmail.com>
2025-03-17 07:26:38 +0000

    test(core): enhance payload parser

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e33b4e5d2d041d0a8395409ddfddd18 = () => {
-  return '2026-03-26T08:12:44Z';
+const node_2917932986b8415d9fc463f4b5019bd3 = () => {
+  return '2025-03-17T07:26:38Z';
 };
