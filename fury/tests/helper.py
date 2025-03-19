b70689123d1f7fa0c803175a9495b4ddc9923345
nyel-101 <dandevjr@gmail.com>
2025-03-19 07:38:52 +0000

    style(db): integrate logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2917932986b8415d9fc463f4b5019bd3 = () => {
-  return '2025-03-17T07:26:38Z';
+const node_9f42b42fcb884a41b3d3a592b67dd04c = () => {
+  return '2025-03-19T07:38:52Z';
 };
