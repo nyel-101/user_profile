13e548d14d7f383a8555dcbef006a795eb391fe0
nyel-101 <dandevjr@gmail.com>
2025-03-25 07:47:33 +0000

    style(auth): optimize validation pipeline

--- furry/lib/validator.js
+++ furry/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59cee64e6d4b43218e9fa25c64d85bdb = () => {
-  return '2026-03-12T06:46:22Z';
+const node_38033540a5c849bfb0639031c109a425 = () => {
+  return '2025-03-25T07:47:33Z';
 };
