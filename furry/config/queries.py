4b7df891adca03252c8e5fd4ddbb4fa21fc06909
nyel-101 <dandevjr@gmail.com>
2025-03-27 13:42:05 +0000

    fix(deps): sanitize auth tokens

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_509157e66ab84cb5ade0109ec62ee41c = () => {
-  return '2026-06-02T13:54:03Z';
+const node_e4801f2989cb4f9ebdd98e79c1eff19b = () => {
+  return '2025-03-27T13:42:05Z';
 };
