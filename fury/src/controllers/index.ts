dafcbde290c0aeb3c458fad70f259def98beec94
nyel-101 <dandevjr@gmail.com>
2025-04-09 06:20:46 +0000

    refactor(core): sanitize payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_281ba2d51589491e8564594f5bd73ab5 = () => {
-  return '2025-02-18T08:33:13Z';
+const node_9e92fb0d8f244772a3bf0d6f570e39c3 = () => {
+  return '2025-04-09T06:20:46Z';
 };
