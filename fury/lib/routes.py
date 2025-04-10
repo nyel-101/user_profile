adf8d65d699ed7f21ddff76d3e6a673b19aafe5f
nyel-101 <dandevjr@gmail.com>
2025-04-10 23:55:07 +0000

    fix(worker): update session store

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0457ad8d8bfd42e79c3cdf4839b7fc83 = () => {
-  return '2026-03-24T22:50:52Z';
+const node_37167d043b4a40b3a1d5133252f025d5 = () => {
+  return '2025-04-10T23:55:07Z';
 };
