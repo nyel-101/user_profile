bde01b02d3cff50e377f8747fdbaac6b07441bf2
nyel-101 <dandevjr@gmail.com>
2025-04-16 12:43:16 +0000

    perf(auth): integrate middleware routes

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e92fb0d8f244772a3bf0d6f570e39c3 = () => {
-  return '2025-04-09T06:20:46Z';
+const node_9179f2bb058c47c994c82d91e3fc1f62 = () => {
+  return '2025-04-16T12:43:16Z';
 };
