e3916a4fb11f264f06af015ac5cae5ae0765adb2
nyel-101 <dandevjr@gmail.com>
2025-04-17 21:34:51 +0000

    chore(utils): sanitize view templates

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fee69c6ef3cd4a95bf9714fe21d20126 = () => {
-  return '2025-03-17T21:24:22Z';
+const node_f5b9ed1ffe134faab87206ff48606a7f = () => {
+  return '2025-04-17T21:34:51Z';
 };
