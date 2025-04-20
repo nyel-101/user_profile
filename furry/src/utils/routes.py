6994d0a77ea2655ca34441eff9bee260d7ea585c
nyel-101 <dandevjr@gmail.com>
2025-04-20 22:01:46 +0000

    feat(api): restructure auth tokens

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_676fbc2489af4addaeb7d371c2d0ccf4 = () => {
-  return '2025-03-23T12:46:01Z';
+const node_6364985621a347fa9db8437c44209716 = () => {
+  return '2025-04-20T22:01:46Z';
 };
