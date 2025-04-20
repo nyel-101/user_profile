0fb1f68d9775959105748c0b83b0a72ba4c1fc0f
nyel-101 <dandevjr@gmail.com>
2025-04-20 15:40:51 +0000

    refactor(utils): fix auth tokens

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cda81c8dcfc4abe8180e17aaab15f91 = () => {
-  return '2025-03-24T04:01:57Z';
+const node_d8ecc90dc7bc4b0da69972cba4fe977e = () => {
+  return '2025-04-20T15:40:51Z';
 };
