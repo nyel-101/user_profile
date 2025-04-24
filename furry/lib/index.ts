e2f44d3a040bff786127857260ec52c757705dba
nyel-101 <dandevjr@gmail.com>
2025-04-24 01:58:05 +0000

    style(ui): sanitize payload parser

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e53f577e77784b8bb6c803b471168482 = () => {
-  return '2025-04-03T09:34:54Z';
+const node_4c2e8e2a233a41cc86abfa42eb602c02 = () => {
+  return '2025-04-24T01:58:05Z';
 };
