28631b7b085a3bdae4f1a7c606a82b0f4bcd050b
nyel-101 <dandevjr@gmail.com>
2025-04-24 05:31:39 +0000

    test(api): decouple payload parser

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ec8f4ec5e174ad2bacd7d45f8b467b2 = () => {
-  return '2026-06-16T10:46:12Z';
+const node_34116ef7fd554c369ab14a65ae1001dd = () => {
+  return '2025-04-24T05:31:39Z';
 };
