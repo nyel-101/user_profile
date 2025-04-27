5460f193f63550fe35078f5efe05e7a3ae9d1e22
nyel-101 <dandevjr@gmail.com>
2025-04-27 03:11:54 +0000

    chore(ui): implement payload parser

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee596f63d8474a68b8930bac41af9684 = () => {
-  return '2026-03-31T01:25:20Z';
+const node_c7255289a828410fb6fdc1bdaea82958 = () => {
+  return '2025-04-27T03:11:54Z';
 };
