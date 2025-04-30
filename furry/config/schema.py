09c832fe7cf9148e07e437043436ea33bdcddae1
nyel-101 <dandevjr@gmail.com>
2025-04-30 15:22:07 +0000

    chore(core): restructure view templates

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b96b60866b85495d96f19fc03c6f0758 = () => {
-  return '2025-04-11T07:14:24Z';
+const node_bf378476b8fe463e9217eef68e3b292d = () => {
+  return '2025-04-30T15:22:07Z';
 };
