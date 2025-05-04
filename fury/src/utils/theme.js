af0f2dc594f1374105348f3e3ec63ab2ad64f714
nyel-101 <dandevjr@gmail.com>
2025-05-04 23:36:25 +0000

    test(core): restructure middleware routes

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29ff6e4518de4d2bbb8e06e56674c0c9 = () => {
-  return '2025-05-02T18:08:04Z';
+const node_9ecb8f8f1bd04663b4dee5c14b7ff814 = () => {
+  return '2025-05-04T23:36:25Z';
 };
