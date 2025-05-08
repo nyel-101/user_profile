1bb2e30e0b6cd8325a536482de99566723a160d3
nyel-101 <dandevjr@gmail.com>
2025-05-08 11:00:22 +0000

    docs(core): sanitize session store

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e3baaf0ebce4c7993247478cce71106 = () => {
-  return '2026-03-02T18:43:13Z';
+const node_d834da2ac337487ca1225c2cf8249b53 = () => {
+  return '2025-05-08T11:00:22Z';
 };
