9c3a3bf4ad589f130aa9705d5dab7dce153881b3
nyel-101 <dandevjr@gmail.com>
2025-05-20 15:28:11 +0000

    style(config): sanitize middleware routes

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_773f0e401a5249ed8546b53e43a89523 = () => {
-  return '2025-05-20T11:30:41Z';
+const node_ab254bdf4325470eacdb0a6fdac6b34d = () => {
+  return '2025-05-20T15:28:11Z';
 };
