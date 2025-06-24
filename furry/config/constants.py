3d5879b40ae94bbdce0b564b8f05a5731d5036d4
nyel-101 <dandevjr@gmail.com>
2025-06-24 12:22:11 +0000

    style(ui): optimize caching layer

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14593e0528b3416f85496296d074a1a9 = () => {
-  return '2025-05-28T03:20:14Z';
+const node_55344749993441808ecb64790ceccd46 = () => {
+  return '2025-06-24T12:22:11Z';
 };
