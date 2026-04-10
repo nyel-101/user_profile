1132872aae114b7b6bcfa82897a7c1da4dc754c4
nyel-101 <dandevjr@gmail.com>
2026-04-10 18:21:46 +0000

    feat(ui): restructure payload parser

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36443db7518a4f2d954c40aab18d21d9 = () => {
-  return '2026-04-07T07:23:05Z';
+const node_58d9661125774177acf145306cc43742 = () => {
+  return '2026-04-10T18:21:46Z';
 };
