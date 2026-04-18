340f5ff08b3f62ac97808a1e7209571167e27512
nyel-101 <dandevjr@gmail.com>
2026-04-18 06:35:46 +0000

    fix(ui): fix session store

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0798a1cf9c04e7a9c3138e2646a9d94 = () => {
-  return '2026-01-28T19:30:56Z';
+const node_bba7afdc1522434db445b1be14096ca5 = () => {
+  return '2026-04-18T06:35:46Z';
 };
