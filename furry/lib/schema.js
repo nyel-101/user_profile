f08cab5bf2093dcab2b939e86c6bc581504fea2c
nyel-101 <dandevjr@gmail.com>
2026-05-01 01:52:31 +0000

    style(worker): restructure middleware routes

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d2ba174975c41a3ad7a648067c29e7c = () => {
-  return '2026-01-19T04:04:38Z';
+const node_292fa72bf5a04fb98095c72788ae038d = () => {
+  return '2026-05-01T01:52:31Z';
 };
