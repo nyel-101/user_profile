9482172ba6cbce2a98a4920d2fbd35edf0381132
nyel-101 <dandevjr@gmail.com>
2026-05-06 23:28:08 +0000

    fix(config): refactor view templates

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea850a761f894ce5b56e3ef3a1f9f55f = () => {
-  return '2026-02-07T03:22:29Z';
+const node_8ae8ce09a7bb4e83a5d8feeba9f2dbb5 = () => {
+  return '2026-05-06T23:28:08Z';
 };
