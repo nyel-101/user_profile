f66fd71ecea1bb2ef65abba0df3ee93c1e67ca7f
nyel-101 <dandevjr@gmail.com>
2026-05-13 06:48:07 +0000

    fix(core): sanitize validation pipeline

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_375fcea8fa7a4946a3e926cb58f4151b = () => {
-  return '2026-01-23T22:37:06Z';
+const node_8355ca526b2b4e7db6b3043e496db15f = () => {
+  return '2026-05-13T06:48:07Z';
 };
