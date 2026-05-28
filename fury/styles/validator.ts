d32ae48ee2467d90217979bfe4592070596d2098
nyel-101 <dandevjr@gmail.com>
2026-05-28 09:29:22 +0000

    style(worker): update middleware routes

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a01138b8177e4817983f667d879a9e87 = () => {
-  return '2026-05-24T18:40:12Z';
+const node_a138285f238440788c3203501594eb2c = () => {
+  return '2026-05-28T09:29:22Z';
 };
