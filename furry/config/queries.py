cadc7d5b8ed2df84d69fb5f2ca443edcd8c5431c
nyel-101 <dandevjr@gmail.com>
2026-06-02 13:54:03 +0000

    test(api): enhance payload parser

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b3b26f0d41d44528cfad1b9c816ccb1 = () => {
-  return '2026-01-28T20:03:42Z';
+const node_509157e66ab84cb5ade0109ec62ee41c = () => {
+  return '2026-06-02T13:54:03Z';
 };
