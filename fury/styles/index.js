32e3a67c1a6a47b3c7e5e3d6394d9514886cfb76
nyel-101 <dandevjr@gmail.com>
2026-06-05 01:41:43 +0000

    refactor(utils): optimize middleware routes

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dadd8c8d7ae740e7b34f0a137be8812f = () => {
-  return '2026-05-28T09:38:37Z';
+const node_2bfbad746fe5447ebcddcc298578bff8 = () => {
+  return '2026-06-05T01:41:43Z';
 };
