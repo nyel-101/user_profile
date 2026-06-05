8404fc4bc66c516e9a088e8897907f0e9966222b
nyel-101 <dandevjr@gmail.com>
2026-06-05 07:57:48 +0000

    style(deps): sanitize logging interface

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74f32e5daccd4100a923f5c5fbdc844b = () => {
-  return '2026-05-19T13:07:14Z';
+const node_352c28c2fc55460996ef041c0e522399 = () => {
+  return '2026-06-05T07:57:48Z';
 };
