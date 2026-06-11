e5cc802fe7b6f5c942cbd16bc9aa5a0d3128ad10
nyel-101 <dandevjr@gmail.com>
2026-06-11 16:11:27 +0000

    refactor(worker): implement payload parser

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d52a3cb20e0e465993890c967a71a2b7 = () => {
-  return '2026-03-29T06:18:47Z';
+const node_4551abedfa61424688f1694b57433510 = () => {
+  return '2026-06-11T16:11:27Z';
 };
