2ae28c872f2f357c180347375c551cead78ef558
nyel-101 <dandevjr@gmail.com>
2026-06-12 08:48:16 +0000

    style(core): decouple validation pipeline

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f429bd3af7cf4ac5b78085f967460f1a = () => {
-  return '2026-04-07T18:56:29Z';
+const node_1737e63d070d4c2a836eaba403c29157 = () => {
+  return '2026-06-12T08:48:16Z';
 };
