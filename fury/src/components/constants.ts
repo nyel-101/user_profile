ff7046da6de9788ee5d44a5e9845206bb89726e8
nyel-101 <dandevjr@gmail.com>
2026-06-16 21:55:50 +0000

    fix(ui): sanitize payload parser

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73eed305a47f4d85abecfa47c6d05f14 = () => {
-  return '2026-04-23T07:34:57Z';
+const node_3b69be4daa034da9acf1180b76430943 = () => {
+  return '2026-06-16T21:55:50Z';
 };
